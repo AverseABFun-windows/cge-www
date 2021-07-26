8d8b5003105dc43c4cf88acc7e3ef16bde28aa89
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-07-27 00:56:21 +0200

    Upgrade to Bootstrap 5 - start

--- htdocs/castle-engine-website-base/kambi_common.php
+++ htdocs/castle-engine-website-base/kambi_common.php
@@ -391,7 +391,6 @@ if ($castle_wordpress) {
 <head>
 <!-- meta suggested by bootstrap, but generally sensible -->
 <meta charset="<?php if ($castle_wordpress) bloginfo('charset'); else echo 'utf-8'; ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="Author" content="Michalis Kamburelis">
 
@@ -442,18 +441,9 @@ if ($castle_wordpress) {
 
 <!-- Bootstrap -->
 <link href="<?php echo page_requisite('castle-engine-website-base/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-<!-- Bootstrap theme -->
-<link href="<?php echo page_requisite('castle-engine-website-base/bootstrap/css/bootstrap-theme.min.css'); ?>" rel="stylesheet">
 
 <!-- Colorbox -->
 <link href="<?php echo page_requisite('castle-engine-website-base/colorbox/example3/colorbox.css'); ?>" type="text/css" rel="stylesheet">
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
 
 <?php
   echo_header_bonus();
@@ -501,16 +491,15 @@ function common_footer($js_using_jquery = '')
   }
 ?>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins).
-     Used also by colorbox. -->
+<!-- jQuery. Used also by colorbox. -->
 <script src="<?php echo page_requisite('castle-engine-website-base/js/jquery.min.js'); ?>" type="text/javascript"></script>
 <!-- Include colorbox after jQuery is known -->
 <script src="<?php echo page_requisite('castle-engine-website-base/colorbox/jquery.colorbox-min.js'); ?>" type="text/javascript"></script>
 <script type="text/javascript">
   jQuery('a.screenshot').colorbox({opacity: 0.9, rel:'screenshot', maxWidth:'90%', maxHeight:'90%'});
 </script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="<?php echo page_requisite('castle-engine-website-base/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<!-- Include Bootstrap, after jQuery (Bootstrap integrates with jQuery if found) -->
+<script src="<?php echo page_requisite('castle-engine-website-base/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 
 <?php
 if ($js_using_jquery) {
